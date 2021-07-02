748443ae7b5b74e1c9f0cce3c0c3a20bf18b29fc
Madeline Whitmore <madeline-whitmore@protonmail.com>
2021-07-02 23:21:56 +1200

    Initial commit

---
+++ IndependentHID.cpp
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+
+#include "bsp/board.h"
+#include "tusb.h"
+
+#include "usb_descriptors.h"
+
+enum
+{
+    BLINK_NOT_MOUNTED = 250,
+    BLINK_MOUNTED = 1000,
+    BLINK_SUSPENDED = 2500,
+};
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+
+void led_blinking_task(void);
+void hid_task(void);
+
+int main()
+{
+    stdio_init_all();
+    printf("Initialising Pi Pico.");
+    board_init();
+    printf(".");
+    tusb_init();
+    printf(".\nInitialised\n");
+
+    while(true)
+    {
+        tud_task();
+        led_blinking_task();
+
+        hid_task();
+    }
+
+    return 0;
+}
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+    blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+    blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+    (void)remote_wakeup_en;
+    blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Involked when usb bus is resumed
+void tud_resume_cb(void)
+{
+    blink_interval_ms = BLINK_MOUNTED;
+}
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+void hid_task(void)
+{
+    // Poll every 10ms
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
+
+    if(board_millis() - start_ms < interval_ms)
+        return;
+    
+    start_ms += interval_ms;
+
+    uint32_t const btn = 0;
+
+    // Remote wakeup
+    if (tud_suspended() && btn)
+    {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
+    }
+
+    /*------------- GamePad -------------*/
+    if (tud_hid_ready())
+    {
+        static bool has_gamepad_key = true;
+
+        hid_gamepad_report_t report = 
+        {
+            .x = 0, .y = 0, .z = 0, .rz = 0, .rx = 0, .ry = 0, .hat = 0, .buttons = 0
+        };
+        
+        if (btn)
+        {
+            report.hat = GAMEPAD_HAT_UP;
+            report.buttons = GAMEPAD_BUTTON_A;
+            tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
+
+            has_gamepad_key = true;
+        }
+        else
+        {
+            report.hat = GAMEPAD_HAT_CENTERED;
+            report.buttons = 0;
+            if (has_gamepad_key)
+                tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
+            has_gamepad_key = false;
+        }
+    }
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+{
+    // TODO not Implemented
+    (void) itf;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
+{
+    // TODO set LED based on CAPLOCK, NUMLOCK etc...
+    (void) itf;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) bufsize;
+}
+
+void led_blinking_task(void)
+{
+    static uint32_t start_ms = 0;
+    static bool led_state = false;
+
+    // Blink every interval ms
+    if (board_millis() - start_ms < blink_interval_ms)
+        return; // not enough time
+    start_ms += blink_interval_ms;
+
+    board_led_write(led_state);
+    led_state = 1 - led_state; // toggle
+}
